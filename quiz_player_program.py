0f177fe38cba9ceb8698599b39bebb1c5ae5c8f4
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-05-01 21:24:44 +0800

    Pseudo Code Draft, 1st Commit

---
+++ quiz_player_program.py
@@ -0,0 +1,10 @@
+# Prompt user to insert txt file to read
+    # Add functionality so that user can still access previous file imported (?)
+        # Multiple txt files and menu to choose which quiz to play
+
+# Read file
+
+# Quizzes the user using the txt file
+    # One question at a time
+    # Randomized number selection
+    # Print score after user answers all question for a txt file. 
