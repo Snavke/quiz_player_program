c409d71ea029173e0a38eba9829ee88345cd1fd2
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-05-04 00:57:32 +0800

    Reverted to read text file line by line to prepare for json conversion.

--- quiz_player.py
+++ quiz_player.py
@@ -11,29 +11,22 @@ quiz_file = filedialog.askopenfilename()
 # Reads file
 if quiz_file:
     with open (quiz_file, 'r') as file:
-        file_content = file.read()
-        print (file_content)
+        lines_text_file= file.readlines()
+        for line in lines_text_file:
+            print(repr(line))
 # Handles cancels to prevent crashes
 else:
     print ("No file selected")
 
 
-with open (quiz_file, "r") as file:
-    for line in file:
-        print (line)
-    # Add functionality so that user can still access previous file imported (?)
-
-        # Multiple txt files and menu to choose which quiz to play
-
-
-
-
-
-
-
-# Read file
-
+    
 # Quizzes the user using the txt file
     # One question at a time
     # Randomized number selection
     # Print score after user answers all question for a txt file. 
+
+
+
+ # Add functionality so that user can still access previous file imported (?)
+
+    # Multiple txt files and menu to choose which quiz to play
