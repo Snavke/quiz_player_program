7301867d81082d25797fceefc2b417e9a99622e3
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-05-05 01:10:04 +0800

    Able to partial parse the txt file contents to be converted to json file, answer line not parsing correctly. Commit 8

--- quiz_player.py
+++ quiz_player.py
@@ -1,6 +1,7 @@
 # Prompt user to insert txt file to read
 import tkinter as tk
 from tkinter import filedialog
+import json
 
 root = tk.Tk()
 root.withdraw()
@@ -8,15 +9,55 @@ root.withdraw()
 # Opens the file diaglog
 quiz_file = filedialog.askopenfilename()
 
-# Reads file
-if quiz_file:
-    with open (quiz_file, 'r') as file:
-        lines_text_file= file.readlines()
-        for line in lines_text_file:
-            print(repr(line))
+# Reads file & Convert Json File to 
+
+quiz_information = {}
+current_question = None
+
+
+with open (quiz_file, 'r') as file:
+    lines_text_file = file.readlines()
+    # skip header information in txt file
+    for line in lines_text_file:
+        line = line.strip()
+
+        if "~" in line or "Created New Quiz at" in line:
+            continue
+       # Scan and detect for new question
+        if line.startswith("Question"):
+            current_question = line.split(":")[0].strip()
+            quiz_information[current_question] = {
+                "Question": line.split(":", 1)[1].strip(),
+                "Choices": {},
+               "Answer": ""
+           }
+        # if its a choice line (A, B, C, D)
+        elif line and line[0] in ["A", "B", "C", "D"]:
+            if "." in line:
+                choice_label = line[0]
+                choice_text = line.split(".", 1)[1].strip()
+                quiz_information[current_question]["Choices"][choice_label] = choice_text
+
+        # if its an answer line
+        elif line.startswith("Answer"):
+            answer = line.split(":", 1)[1].strip()
+            quiz_information[current_question]["Answer"] = answer
+            print(f"Detected answer for {current_question}: {answer}")
+   
+
+
+print("\nFinal quiz dictionary:\n")
+print(json.dumps(quiz_information, indent=4))
+
+
+    
+
+    
+
+
+
+ 
 # Handles cancels to prevent crashes
-else:
-    print ("No file selected")
 
 
     
